cf6d3527139445aa5be07cea885b79a5201723f0
sein <hyuntpdls@gmail.com>
2022-05-20 22:05:41 +0900

    이미지 캐루셀 구현하기(react-slick)

--- frontend/components/PostImages.js
+++ frontend/components/PostImages.js
@@ -1,18 +1,24 @@
 import { PlusOutlined } from "@ant-design/icons";
 import PropTypes from "prop-types";
 import { useCallback, useState } from "react";
-
+import ImagesZoom from "./imagesZoom";
 export default function PostImages({images}) {
     const [showImagesZoom, setShowImagesZoom] = useState(false);
 
     const onZoom = useCallback(() => {
         setShowImagesZoom(true);
     }, []);
+    
+    const onClose = useCallback(() => {
+        setShowImagesZoom(false);
+    }, []);
+
     if (images.length === 1){
         return(
             <>
             {/* role="presentation" 클릭할 필요없다.  */}
                 <img role="presentation" src={images[0].src} alt={images[0].src} onClick={onZoom} />
+                {showImagesZoom && <ImagesZoom images={images} onClose={onClose} /> }
             </>
         );
     }
@@ -21,10 +27,13 @@ export default function PostImages({images}) {
             <>
                 <img role="presentation" style={{ width: "50%", display: 'inline-block'}} src={images[0].src} alt={images[0].src} onClick={onZoom} />
                 <img role="presentation" style={{ width: "50%", display: 'inline-block'}} src={images[1].src} alt={images[1].src} onClick={onZoom} />
+                {showImagesZoom && <ImagesZoom images={images} onClose={onClose} /> }
+
             </>
         )
     }
     return(
+        <>
         <div>
             <img role="presentation" width="50%" src={images[0].src} alt={images[0].src} onClick={onZoom}/>
             <div
@@ -38,7 +47,9 @@ export default function PostImages({images}) {
                 개의 사진 더 보기 
             </div>
         </div>
+         {showImagesZoom && <ImagesZoom images={images} onClose={onClose} /> }
 
+         </>
     )
 }
 PostImages.propTypes = {
